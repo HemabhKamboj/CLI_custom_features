051ec767fb5c92dac7e35d727bfca9177568544e
root <root@localhost.localdomain>
2018-06-18 22:48:10 +0530

    quotes and wallpaper web scrapping added

--- packages/newsapi.py
+++ packages/newsapi.py
@@ -3,7 +3,6 @@ from pprint import pprint
 
 res = requests.get('https://ipinfo.io/')
 data = res.json()
-print(data)
 country = data['country']
 url = 'https://newsapi.org/v2/top-headlines?''country={}&''apiKey=29cc4d194a15470db46f37d7f30a5178'.format(country)
 response = requests.get(url)
---
+++ packages/wallpaperwebscrapping.py
@@ -0,0 +1,32 @@
+from urllib import request
+from bs4 import BeautifulSoup
+from datetime import datetime as dt
+import requests,os
+
+
+def get_image():
+	res=requests.get('https://bingwallpaper.com/')
+	soup=BeautifulSoup(res.text,"lxml")
+	image=soup.find('a',{'class':'cursor_zoom'}).find('img')
+	url=image.get('src')
+	return url
+
+def download():
+	url=get_image()
+	file_name = dt.now().strftime("%Y-%m-%d")
+	user = os.getenv('USER')
+	path='/root/Pictures/BingWallpapers'
+	full_path=os.path.join(path,file_name)
+	
+
+	with open(full_path, 'wb') as f:
+		f.write(request.urlopen(url).read())
+	return full_path
+
+download()
+
+def change():
+	full_path=download()
+	os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///"+full_path)
+
+change()
